89a901fde29014a476bdbf18cde1d96982df4133
Danielle-El-Murr <98666722+Danielle-El-Murr@users.noreply.github.com>
2023-05-07 23:05:32 +0300

    perfection

--- src/utils/QuestionCard.jsx
+++ src/utils/QuestionCard.jsx
@@ -1,23 +1,30 @@
 import { useEffect, useState } from "react";
 import ConfirmationModal from "./ConfirmationModal";
 import {
+  addDoc,
   collection,
   deleteDoc,
   doc,
   getDoc,
   getDocs,
+  onSnapshot,
   query,
+  setDoc,
   where,
 } from "firebase/firestore";
 import { useParams } from "react-router-dom";
 import Discussions from "../pages/Main/Discussions";
 import { db } from "../Config";
 import { SessionService } from "../SessionService";
+import ReportModal from "./ReportModal";
 
 const QuestionCard = ({ question, onClick, setCurrentComponent }) => {
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [isReportModalOpen, setIsReportModalOpen] = useState(false);
   const [profile, setProfile] = useState();
+  const [type, setType] = useState();
   const [userName, setUserName] = useState();
+  const [isReported, setIsReported] = useState(false);
 
   const uid = useParams().uid;
   const user = SessionService.getUser();
@@ -46,7 +53,40 @@ const QuestionCard = ({ question, onClick, setCurrentComponent }) => {
       setProfile(user.data().profile);
       setUserName(user.data().name);
     });
+
+    onSnapshot(
+      doc(
+        db,
+        "collaborations",
+        uid,
+        "discussions",
+        question.id,
+        "reports",
+        user.id
+      ),
+      (snapshot) => {
+        setIsReported(snapshot.data() === undefined ? false : true);
+      }
+    );
   }, []);
+
+  const handleReport = (e) => {
+    e.preventDefault();
+    console.log(type);
+    addDoc(collection(db, "reports"), { collabId: uid, discId: question.id });
+    setDoc(
+      doc(
+        db,
+        "collaborations",
+        uid,
+        "discussions",
+        question.id,
+        "reports",
+        user.id
+      ),
+      { type: type }
+    );
+  };
 
   return (
     <>
@@ -65,7 +105,7 @@ const QuestionCard = ({ question, onClick, setCurrentComponent }) => {
             )}
             <h1>{userName}</h1>
           </div>
-          <div className="flex gap-4">
+          <div className="flex gap-4 items-center">
             {question.createdAt && (
               <div className="flex gap-1 items-center text-gray-400/75 text-xs	">
                 {question &&
@@ -100,6 +140,43 @@ const QuestionCard = ({ question, onClick, setCurrentComponent }) => {
                   />
                 </svg>
               </span>
+            )}
+            {/* Report button unfilled */}
+
+            {!isReported && (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-5 h-5 hover:text-red-600"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  setIsReportModalOpen(true);
+                }}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3 3v1.5M3 21v-6m0 0l2.77-.693a9 9 0 016.208.682l.108.054a9 9 0 006.086.71l3.114-.732a48.524 48.524 0 01-.005-10.499l-3.11.732a9 9 0 01-6.085-.711l-.108-.054a9 9 0 00-6.208-.682L3 4.5M3 15V4.5"
+                />
+              </svg>
+            )}
+            {/* Report button filled */}
+            {isReported && (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="red"
+                className="w-5 h-5"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M3 2.25a.75.75 0 01.75.75v.54l1.838-.46a9.75 9.75 0 016.725.738l.108.054a8.25 8.25 0 005.58.652l3.109-.732a.75.75 0 01.917.81 47.784 47.784 0 00.005 10.337.75.75 0 01-.574.812l-3.114.733a9.75 9.75 0 01-6.594-.77l-.108-.054a8.25 8.25 0 00-5.69-.625l-2.202.55V21a.75.75 0 01-1.5 0V3A.75.75 0 013 2.25z"
+                  clipRule="evenodd"
+                />
+              </svg>
             )}
           </div>
         </div>
@@ -166,6 +243,12 @@ const QuestionCard = ({ question, onClick, setCurrentComponent }) => {
         isOpen={isDeleteModalOpen}
         setIsOpen={setIsDeleteModalOpen}
         onClick={(e) => handleConfirmDeleteModal(e)}
+      />
+      <ReportModal
+        isOpen={isReportModalOpen}
+        setIsOpen={setIsReportModalOpen}
+        onClick={(e) => handleReport(e)}
+        setType={setType}
       />
     </>
   );
